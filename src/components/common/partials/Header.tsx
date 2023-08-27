930163d45740073ffad00a6f222d237d7298d3d4
naruto1031 <naruto0614@au.com>
2023-08-27 19:40:33 +0900

    update: フォントサイズを変更

--- src/components/common/partials/Header.tsx
+++ src/components/common/partials/Header.tsx
@@ -4,7 +4,7 @@ import { hstack } from "../../../../styled-system/patterns"
 export const Header = () => {
 	return (
 		<header className={hstack({height: "80px", padding: "24px", backgroundColor: "bgWhite"})}>
-			<div className={css({ color: "fontBlack" ,fontFamily: "Inter", fontSize: "2xl", fontWeight: "bold"})}><span className={css({color: "fontRed"})}>Re</span>collect</div>
+			<div className={css({ color: "fontBlack" ,fontFamily: "Inter", fontSize: "3xl", fontWeight: "bold"})}><span className={css({color: "fontRed"})}>Re</span>collect</div>
 		</header>
 	)
 }
