24a0355e843d50ada83c3fd5efc0ad2bd5c85f1f
naruto1031 <naruto0614@au.com>
2023-08-27 17:22:04 +0900

    feat: ヘッダーとフッターを作成

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { css } from '../../styled-system/css'
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
-      <body>
+      <body className={css({backgroundColor: "bgDefault"})}>
         <Main />
         <NextScript />
       </body>
