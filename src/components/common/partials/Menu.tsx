6cddcd9316e8e821257d5bdaed5847a97385217a
seiya_tagami <bepozurapu274@gmail.com>
2023-09-01 11:48:33 +0900

    feat: メニューを実装

--- src/components/common/partials/Menu.tsx
+++ src/components/common/partials/Menu.tsx
@@ -5,44 +5,112 @@ import { center } from '../../../../styled-system/patterns'
 import { motion } from 'framer-motion'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRightFromBracket, faCompass, faPenToSquare, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export const Menu: FC = () => {
   const [active, setActive] = useState(false)
+  const handleMenu = () => {
+    setActive(prev => !prev)
+  }
   return (
-    <motion.div
-      className={css({ pos: 'fixed', right: '100px', bottom: '80px', cursor: 'pointer' })}
-      whileTap={{ scale: 0.9 }}
-    >
-      <div className={`menu ${active && 'active'}`}>
-        <div
+    <div className={css({
+      pos: "fixed",
+      right: "70px",
+      bottom: "70px",
+    })}>
+      <div className={css({
+        pos: "relative",
+        w: '80px',
+        h: '80px',
+      })}>
+        <motion.div
           className={center({
-            pos: 'absolute',
+            pos: "absolute",
             zIndex: 1000,
             w: '80px',
             h: '80px',
             bg: 'dimBlue',
             rounded: 'full',
             shadow: 'xl',
-            transform: '1.25s',
+            cursor: 'pointer'
           })}
-          onClick={() => setActive(!active)}
+          whileTap={{ scale: 0.9 }}
+          onClick={handleMenu}
         >
-          <FontAwesomeIcon
-            icon={faBars}
-            style={{ width: '30px', height: '30px', color: 'white' }}
-          />
-        </div>
-        <li>
-          <Link href={'/'}>A</Link>
-        </li>
-        <li>
-          <Link href={'/'}>B</Link>
-        </li>
-        <li>
-          <Link href={'/'}>C</Link>
-        </li>
+          <FontAwesomeIcon icon={faCompass} style={{ width: "42px", height: "42px", color: "white" }} />
+        </motion.div>
+        <Link href={'/'} title='自分史を作成'>
+          <motion.li className={center({
+            pos: "absolute",
+            top: 1,
+            left: 1,
+            w: '72px',
+            h: '72px',
+            bg: 'white',
+            rounded: 'full',
+            listStyle: 'none',
+            shadow: 'xl'
+          })}
+            whileTap={{ scale: 0.9 }}
+            animate={active ? {
+              x: -0,
+              y: -140
+            } : {
+              x: 0,
+              y: 0
+            }}
+          >
+            <FontAwesomeIcon icon={faPlus} style={{ width: "32px", height: "32px", color: "#0C4C97" }} />
+          </motion.li>
+        </Link>
+        <Link href={'/'} title='自分史を編集'>
+          <motion.li className={center({
+            pos: "absolute",
+            top: 1,
+            left: 1,
+            w: '72px',
+            h: '72px',
+            bg: 'white',
+            rounded: 'full',
+            listStyle: 'none',
+            shadow: 'xl'
+          })}
+            whileTap={{ scale: 0.9 }}
+            animate={active ? {
+              x: -100,
+              y: -100
+            } : {
+              x: 0,
+              y: 0
+            }}
+          >
+            <FontAwesomeIcon icon={faPenToSquare} style={{ width: "32px", height: "32px", color: "#0C4C97" }} />
+          </motion.li>
+        </Link>
+        <Link href={'/'} title='ログアウト'>
+          <motion.li
+            className={center({
+              pos: "absolute",
+              top: 1,
+              left: 1,
+              w: '72px',
+              h: '72px',
+              bg: 'white',
+              rounded: 'full',
+              listStyle: 'none',
+              shadow: 'xl'
+            })}
+            whileTap={{ scale: 0.9 }}
+            animate={active ? {
+              x: -140
+            } : {
+              x: 0
+            }}
+          >
+            <FontAwesomeIcon icon={faArrowRightFromBracket} style={{ width: "32px", height: "32px", color: "#0C4C97" }} />
+          </motion.li>
+        </Link>
       </div>
-    </motion.div>
+    </div>
   )
 }
