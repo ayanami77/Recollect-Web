830d2fec3e1d3dd2b69ca942b6c9e2aad3dacdbb
seiya_tagami <bepozurapu274@gmail.com>
2023-09-05 13:57:59 +0900

    update: サインアップとログインのUIのブラッシュアップ

--- src/components/auth/partials/SignUpForm.tsx
+++ src/components/auth/partials/SignUpForm.tsx
@@ -3,7 +3,6 @@ import { css } from '../../../../styled-system/css'
 import { hstack, vstack } from '../../../../styled-system/patterns'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { AuthValidationSchema, AuthValidationSchemaType } from '@/libs/validations/authValidation'
-import { ReactNode } from 'react'
 import { useMutateUser } from '@/api/hooks/user/useMutateUser'
 
 export const SignUpForm = () => {
@@ -36,55 +35,59 @@ export const SignUpForm = () => {
     >
       <div className={vstack({ alignItems: 'start', w: 'full' })}>
         <h2 className={css({ fontSize: '2xl', fontWeight: 'bold' })}>登録</h2>
-        <div className={vstack({ alignItems: 'start', w: 'full' })}>
-          <label className={hstack({ fontSize: 'xs' })}>
-            <span>ユーザーID</span>
-            <span className={css({ color: 'cinnabar' })}>必須</span>
-          </label>
-          <input
-            type='text'
-            className={css({
-              w: 'full',
-              p: '6px',
-              rounded: 'md',
-              borderColor: 'dimgray',
-              borderWidth: '1px',
-              bg: 'slate.100',
-              _placeholder: {
-                fontSize: 'xs',
-              },
-            })}
-            placeholder='e.g. taro1123'
-            {...register('userId')}
-          />
-          <p className={css({ fontSize: 'xs', color: 'cinnabar' })}>
-            {errors.userId?.message as ReactNode}
-          </p>
-        </div>
-        <div className={vstack({ alignItems: 'start', w: 'full' })}>
-          <label className={hstack({ fontSize: 'xs' })}>
-            <span>パスワード</span>
-            <span className={css({ color: 'cinnabar' })}>必須</span>
-          </label>
-          <input
-            type='password'
-            className={css({
-              w: 'full',
-              p: '6px',
-              rounded: 'md',
-              borderColor: 'dimgray',
-              borderWidth: '1px',
-              bg: 'slate.100',
-              _placeholder: {
-                fontSize: 'xs',
-              },
-            })}
-            placeholder='半角英数字6文字以上'
-            {...register('password')}
-          />
-          <p className={css({ fontSize: 'xs', color: 'cinnabar' })}>
-            {errors.password?.message as ReactNode}
-          </p>
+        <div className={vstack({ alignItems: 'start', gap: '18px', w: 'full' })}>
+          <div className={vstack({ alignItems: 'start', w: 'full' })}>
+            <label className={hstack({ fontSize: '14px' })}>
+              <span>ユーザーID</span>
+              <span className={css({ color: 'cinnabar' })}>必須</span>
+            </label>
+            <input
+              type='text'
+              className={css({
+                w: 'full',
+                p: '6px',
+                rounded: 'md',
+                borderColor: 'dimgray',
+                borderWidth: '1px',
+                bg: 'slate.100',
+                _placeholder: {
+                  fontSize: 'xs',
+                },
+              })}
+              placeholder='e.g. taro1123'
+              {...register('userId')}
+            />
+            {errors.userId?.message && (
+              <p className={css({ fontSize: 'xs', color: 'cinnabar' })}>{errors.userId.message}</p>
+            )}
+          </div>
+          <div className={vstack({ alignItems: 'start', w: 'full' })}>
+            <label className={hstack({ fontSize: '14px' })}>
+              <span>パスワード</span>
+              <span className={css({ color: 'cinnabar' })}>必須</span>
+            </label>
+            <input
+              type='password'
+              className={css({
+                w: 'full',
+                p: '6px',
+                rounded: 'md',
+                borderColor: 'dimgray',
+                borderWidth: '1px',
+                bg: 'slate.100',
+                _placeholder: {
+                  fontSize: 'xs',
+                },
+              })}
+              placeholder='半角英数字6文字以上'
+              {...register('password')}
+            />
+            {errors.password?.message && (
+              <p className={css({ fontSize: 'xs', color: 'cinnabar' })}>
+                {errors.password.message}
+              </p>
+            )}
+          </div>
         </div>
         <button
           className={css({
