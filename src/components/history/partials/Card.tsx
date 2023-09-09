cc04fb6a255fe1689061619f202fca2af3936c93
seiya_tagami <bepozurapu274@gmail.com>
2023-09-09 12:22:37 +0900

    feat: detailModalオープン時に後ろ側をスクロールさせないようにした

--- src/components/history/partials/Card.tsx
+++ src/components/history/partials/Card.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { Tag } from '@/components/common'
 import { Period as TPeriod } from '@/api/models'
 import { CardDetailModal } from './CardDetailModal'
+import { controlScroll } from '@/utils/controlScroll'
 
 type CardProps = {
   data: {
@@ -22,7 +23,10 @@ type CardProps = {
 export const Card: FC<CardProps> = (props) => {
   const { data } = props
   const [isOpen, setIsOpen] = useState(false)
-  const handleOpen = () => setIsOpen((prev) => !prev)
+  const handleOpen = () => {
+    controlScroll(isOpen)
+    setIsOpen((prev) => !prev)
+  }
 
   return (
     <>
