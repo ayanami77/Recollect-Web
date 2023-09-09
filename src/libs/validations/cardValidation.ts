52ac4758b78477d900088c325516f83cc7bc9d5d
seiya_tagami <bepozurapu274@gmail.com>
2023-09-09 15:36:24 +0900

    fix: contentのバリデーションを取っ払った

--- src/libs/validations/cardValidation.ts
+++ src/libs/validations/cardValidation.ts
@@ -2,6 +2,6 @@ import { z } from 'zod'
 
 export const CardValidationSchema = z.object({
   title: z.string().nonempty('タイトルは必須です。').max(20, '最大20文字です。'),
-  content: z.string().nonempty('内容は必須です。'),
+  content: z.string(),
 })
 export type TCardValidationSchema = z.infer<typeof CardValidationSchema>
