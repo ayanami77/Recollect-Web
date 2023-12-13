1309498d302dec73b4049224edf7a913b974d9f4
seiya_tagami <bepozurapu274@gmail.com>
2023-12-13 20:09:42 +0900

    refac: footerの調整

--- src/components/layouts/GlobalLayout.tsx
+++ src/components/layouts/GlobalLayout.tsx
@@ -1,15 +1,15 @@
-import { Footer, Header } from '@/components/common'
+import { Header } from '@/components/common'
+import { Noto_Sans_JP } from 'next/font/google'
 import { FC, ReactNode } from 'react'
 import { css } from '../../../styled-system/css'
 
-// const notesansjp = Noto_Sans_JP({ subsets: ['latin'] })
+const notesansjp = Noto_Sans_JP({ subsets: ['latin'] })
 
 const GlobalLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div>
+    <div className={notesansjp.className}>
       <Header />
       <main className={css({ mt: '80px', md: { mt: '100px' } })}>{children}</main>
-      <Footer />
     </div>
   )
 }
