78f51cf20de0638a3b5186927a7c90c2551be452
naruto1031 <naruto0614@au.com>
2023-12-15 01:29:18 +0900

    update: レイアウトの修正

--- src/components/common/partials/Header.tsx
+++ src/components/common/partials/Header.tsx
@@ -39,7 +39,7 @@ export const Header = () => {
           zIndex: 10,
         })}
       >
-        <Link href={router.pathname === '/signin' || '/' ? '/' : '/history'}>
+        <Link href={router.pathname === '/signin' || router.pathname === '/' ? '/' : '/history'}>
           <div
             className={css({
               md: { w: '200px', h: '80px' },
